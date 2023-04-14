e82960799490db08a4d9b2ce7cc6935b84c5e2f5
mohon63 <mohonhossen63@gmail.com>
2023-04-14 16:12:55 +0600

    vercel

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ async function run() {
 
         app.put('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = { _id: ObjectId(id) }
+            const filter = { _id: new ObjectId(id) }
             const options = { upsert: true };
             const updatedDoc = {
                 $set: {
